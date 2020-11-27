52cdad2bdb2b4f8f98ea7cab506fb2ce8683023a
Abdulsamii Ajala <kgasta@gmail.com>
2020-11-27 17:11:56 +0100

    Created a new Expo app

---
+++ app/App.js
@@ -0,0 +1,18 @@
+import { StatusBar as ExpoStatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <ExpoStatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  topBar: {},
+});
